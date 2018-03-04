a6d54514514fe184f0d22f3ce941481ef58d5b7f
Jonathan Rudenberg <jonathan@titanous.com>
2018-03-03 21:05:22 -0500

    Fix bypass
    This fixes a trivial bypass via `delete navigator.bluetooth`
    
    Reported in titanous/disable-webusb#1

--- disable_webbluetooth.js
+++ disable_webbluetooth.js
@@ -1,3 +1,8 @@
 var script = document.createElement('script');
-script.text = '{ navigator.__defineGetter__("bluetooth", () => undefined); }';
+script.text = `
+if (navigator.bluetooth) {
+  delete navigator.bluetooth.__proto__.requestDevice;
+  navigator.__defineGetter__("bluetooth", () => undefined);
+}
+`;
 document.documentElement.appendChild(script);
