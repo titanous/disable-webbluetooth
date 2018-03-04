e01f9fb7a9d09a8935bd799a431b61d16f1d3e02
Jonathan Rudenberg <jonathan@titanous.com>
2018-03-03 21:44:27 -0500

    Improve blocker
    bluetooth is on navigator.__proto__ and the deletion appears to stick.

--- disable_webbluetooth.js
+++ disable_webbluetooth.js
@@ -2,7 +2,7 @@ var script = document.createElement('script');
 script.text = `
 if (navigator.bluetooth) {
   delete navigator.bluetooth.__proto__.requestDevice;
-  navigator.__defineGetter__("bluetooth", () => undefined);
+  delete navigator.__proto__.bluetooth;
 }
 `;
 document.documentElement.appendChild(script);
